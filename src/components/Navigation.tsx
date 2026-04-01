ab9bc865934143d9944592ed51ab68d08fd6f82b
Filip Kasaj <filip.kasaj@gmail.com>
2026-04-01 22:31:44 +0200

    icon time

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -40,7 +40,10 @@ export default function Navigation({ currentPage, onNavigate }: NavigationProps)
         </NavButton>
 
         <NavButton page="time" currentPage={currentPage} onNavigate={onNavigate}>
-          <span className="text-2xl">📅</span>
+          <svg className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5}
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>
         </NavButton>
 
         <NavButton page="info" currentPage={currentPage} onNavigate={onNavigate}>
